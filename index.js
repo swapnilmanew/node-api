2ac20329eea0f5e0ea6301cebab9f5e3feae44fa
swapnilmanew <swapnilcodes26@gmail.com>
2022-09-11 05:40:56 +0530

    added CORS middleware | node js app

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose')
+const cors = require("cors")
 require('dotenv').config()
 mongoose.connect(process.env.URL)
 .then((response)=>console.log("connected successfully"))
 .catch((err) =>console.log(err))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
-
-
+app.use(cors());
 // routes
 const animeRoutes = require("./routes/anime_routes")
 app.use("/api/anime",animeRoutes)
