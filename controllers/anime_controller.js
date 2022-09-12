404d15be1edae509177c736a86a95b51a5fdbc19
swapnilmanew <swapnilcodes26@gmail.com>
2022-09-12 21:55:21 +0530

    formatted | - Prettier

--- controllers/anime_controller.js
+++ controllers/anime_controller.js
@@ -1,79 +1,27 @@
-const Anime = require("../models/anime_model")
+const Anime = require("../models/anime_model");
 class AnimeControllers {
-    // getting all animes
-    static index = async(req,res) =>
-    {
-      try {
-        const AnimeData = await Anime.find({});
-        res.json(AnimeData);
-      } catch (error) {
-            console.log(error)
-      }
+  // getting all animes
+  static index = async (req, res) => {
+    try {
+      const AnimeData = await Anime.find({});
+      res.json(AnimeData);
+    } catch (error) {
+      console.log(error);
     }
-
-    // storing an anime
-    static store = async(req,res) =>
-    {
-        const {
-            anime_name,
-            episodes,
-            released_year,
-            seasons,
-            anime_category,
-            anime_description,
-            anime_thumbnail,
-          } = req.body;
-      try {
-        const AnimeData = await new Anime({
-            anime_name: anime_name,
-            anime_episodes: episodes,
-            anime_released_year: released_year,
-            anime_seasons: seasons,
-            anime_category: anime_category,
-            anime_description: anime_description,
-            anime_thumbnail: anime_thumbnail,
-        });
-        const fr = await AnimeData.save();
-        return res.json({
-            "msg" : "Anime created successfully",
-            "status_code": 1,
-            "anime_data" : fr,
-        });
-      } catch (error) {
-        return res.json({
-            "msg" : "Something went wrong !",
-            "status_code": 1,
-            "anime_data" : [],
-        });
-      }
-    }
-    //  showing an anime
-    static show = async(req,res) =>
-    {
-      const _id = req.params._id;
-      try {
-        const AnimeData = await Anime.findOne({_id});
-        res.json(AnimeData);
-      } catch (error) {
-            console.log(error)
-      }
-    }
-     //  Updating an anime
-     static update = async(req,res) =>
-     {
-      const {
-        anime_id,
-        anime_name,
-        episodes,
-        released_year,
-        seasons,
-        anime_category,
-        anime_description,
-        anime_thumbnail,
-      } = req.body;
-  try {
-    const AnimeData = await Anime.findByIdAndUpdate(anime_id,
-       {
+  };
+  // storing an anime
+  static store = async (req, res) => {
+    const {
+      anime_name,
+      episodes,
+      released_year,
+      seasons,
+      anime_category,
+      anime_description,
+      anime_thumbnail,
+    } = req.body;
+    try {
+      const AnimeData = await new Anime({
         anime_name: anime_name,
         anime_episodes: episodes,
         anime_released_year: released_year,
@@ -81,26 +29,71 @@ class AnimeControllers {
         anime_category: anime_category,
         anime_description: anime_description,
         anime_thumbnail: anime_thumbnail,
-       }
-       );
-    const fr = await AnimeData.save();
-    return res.json({
-        "msg" : "Anime updated successfully",
-        "status_code": 1,
-        "anime_data" : fr,
-    });
-  } catch (error) {
-    return res.json({
-        "msg" : "Something went wrong !",
-        "status_code": 1,
-        "anime_data" : [],
-    });
-  }
-     }
+      });
+      const fr = await AnimeData.save();
+      return res.json({
+        msg: "Anime created successfully",
+        status_code: 1,
+        anime_data: fr,
+      });
+    } catch (error) {
+      return res.json({
+        msg: "Something went wrong !",
+        status_code: 1,
+        anime_data: [],
+      });
+    }
+  };
+  //  showing an anime
+  static show = async (req, res) => {
+    const _id = req.params._id;
+    try {
+      const AnimeData = await Anime.findOne({ _id });
+      res.json(AnimeData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //  Updating an anime
+  static update = async (req, res) => {
+    const {
+      anime_id,
+      anime_name,
+      episodes,
+      released_year,
+      seasons,
+      anime_category,
+      anime_description,
+      anime_thumbnail,
+    } = req.body;
+    try {
+      const AnimeData = await Anime.findByIdAndUpdate(anime_id, {
+        anime_name: anime_name,
+        anime_episodes: episodes,
+        anime_released_year: released_year,
+        anime_seasons: seasons,
+        anime_category: anime_category,
+        anime_description: anime_description,
+        anime_thumbnail: anime_thumbnail,
+      });
+      const fr = await AnimeData.save();
+      return res.json({
+        msg: "Anime updated successfully",
+        status_code: 1,
+        anime_data: fr,
+      });
+    } catch (error) {
+      return res.json({
+        msg: "Something went wrong !",
+        status_code: 1,
+        anime_data: [],
+      });
+    }
+  };
 }
 module.exports = {
-    index : AnimeControllers.index,
-    store : AnimeControllers.store,
-    show : AnimeControllers.show,
-    update : AnimeControllers.update,
-}
+  index: AnimeControllers.index,
+  store: AnimeControllers.store,
+  show: AnimeControllers.show,
+  update: AnimeControllers.update,
+};
--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-const cors = require("cors")
-require('dotenv').config()
-mongoose.connect(process.env.URL)
-.then((response)=>console.log("connected successfully"))
-.catch((err) =>console.log(err))
-app.use(express.json())
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+mongoose
+  .connect(process.env.URL)
+  .then((response) => console.log("connected successfully"))
+  .catch((err) => console.log(err));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 // routes
-const animeRoutes = require("./routes/anime_routes")
-app.use("/api/anime",animeRoutes)
+const animeRoutes = require("./routes/anime_routes");
+app.use("/api/anime", animeRoutes);
 
 // season
-const seasonRoute = require("./routes/season_routes")
-app.use("/api/season",seasonRoute)
+const seasonRoute = require("./routes/season_routes");
+app.use("/api/season", seasonRoute);
 
 // episode route
-const episodeRoute = require("./routes/episode_routes")
-app.use("/api/episode",episodeRoute)
-
-app.listen(3000, ()=>{
-    console.log("server is running")
-})
+const episodeRoute = require("./routes/episode_routes");
+app.use("/api/episode", episodeRoute);
+app.listen(3000, () => {
+  console.log("server is running");
+});
